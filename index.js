00f8d9853e56e1fa1ea5a5f3ff19f5c5837840a2
rax <raxpost@gmail.com>
2016-12-17 15:45:05 +0100

    dbs

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
+// @flow
+
 const cheerio = require('cheerio');
 const pgp = require('pg-promise')();
 const rp = require('request-promise');
 const url = 'http://www2.hm.com/ru_ru/index.html';
 const catRgxp = 'ru_ru/(muzhchiny|zhenshchiny)/(.*?)/(.*?)\.html'; 
 const shopName = 'H&M';
-const db = pgp('postgres://rax:@locahost:port/database');
+const db = pgp('postgres://rax:@localhost:5432/shops');
 
-function getShopId() {
-
+function getShopId(shopName) {
+  return db.one('select id from shops where name=${shopName}', {shopName}).then(res => {
+    return res.id;
+  });
 }
 
 function createOrUpdateCat(shopName, name, url) {
 
 }
 
-
-rp({
-  url
-}).then(res => {
-  const $ = cheerio.load(res);
-  $('a').each(function (i, elem) {
-    const link = $(this).attr('href');
-    const text = $(this).text().trim();
-    const r = new RegExp(catRgxp);
-    const m = link.match(r); 
-    if (m) {
-      console.log(m[3], link, text);
-    }
+function run() {
+  rp({
+    url
+  }).then(res => {
+    const $ = cheerio.load(res);
+    $('a').each(function (i, elem) {
+      const link = $(this).attr('href');
+      const text = $(this).text().trim();
+      const r = new RegExp(catRgxp);
+      const m = link.match(r); 
+      if (m) {
+        console.log(m[3], link, text);
+      }
+    });
   });
+}
 
-});
+getShopId('h&m').then(console.log).catch(console.log);
